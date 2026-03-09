9bcdf976426dd2f891f7b1ae611699f25a2916ee
ArciWeb <Freeuser0@proton.me>
2026-03-09 15:21:24 +0100

    Add files via upload

--- arci-map.js
+++ arci-map.js
@@ -20,7 +20,7 @@ const buildingsData = {
         "casino": { name: "ArciBet Casino", x: 78.5, y: 82.5, doorX: 78.5, doorY: 82.5, size: 105, img: "buda_arcibet.png", detail: "mapa-arcibet.png", action: "openArciCasino" },
     "tip": { name: "ArciTip Stávková", x: 66.7, y: 83.5, doorX: 66.7, doorY: 83.5, size: 95, img: "buda_arcitip.png", detail: "mapa-tip.png", action: "openArciTip" },
     
-    "oblubene": { name: "Obľúbený Hrači", x: 13.3, y: 86.8, doorX: 58.0, doorY: 88.6, size: 80, img: "buda_oblubene.png", detail: "mapa-oblubene.png", action: "toggleFavoritesWidget" },
+    "oblubene": { name: "Obľúbený Hrači", x: 13.3, y: 86.8, doorX: 58.0, doorY: 88.6, size: 80, img: "buda_oblubene.png", detail: "mapa-oblubene.png", action: "openFavoritesModal" },
     
     "osobne": { name: "Osobné úspechy", x: 13.3, y: 86.8, doorX: 9.3, doorY: 77.8, size: 160, img: "buda_zahradka.png", detail: "mapa-personal.png", action: "openPersonalAchievements" },
     "plavaren": { name: "História Spokojnosti", x: 89.9, y: 62.0, doorX: 89.9, doorY: 62.0, size: 162, img: "buda_plavaren.png", detail: "mapa-happy.png", action: "openHappinessOverview" },
